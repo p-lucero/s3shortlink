f42e14c423605b8d2ab1d99a75148b32771111ca
p-lucero <p-lucero@users.noreply.github.com>
2018-12-01 10:11:07 -0700

    Add baseline files for Python package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="s3-shortlink",
+    version="0.0.1",
+    author="Paul Lucero",
+    author_email="paul.lucero08@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/s3-shortlink",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
